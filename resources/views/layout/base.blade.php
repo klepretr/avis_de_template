d25ddf73e17de44c08111d48cf7d52d7da617a92
Kévin Leprêtre <kevin.lepretre@insa-cvl.fr>
2017-12-07 21:02:13 +0100

    Gabarit pour les premières vues

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,14 @@
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Safety - @yield('title')</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    </head>
+    <body>
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/trafic', function () {
+    return view('trafic.timeline');
+});
+
+Route::get('/trafic/map', function () {
+    return view('trafic.map');
+});
