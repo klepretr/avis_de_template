e0ee562852bdf6dbe280c0af86099d0748462bee
jiroco <jean.savoldelli@insa-cvl.fr>
2017-12-08 00:00:36 +0100

    good

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 /**
 * 
 */
 class EventsController extends Controller
 {
+	
+	public function index(){
+		return view('outlings');
+	}
+	public function show_create_outling(){
+		return view('create_outling');
+	}
+
+	
+	public function create_outling(Request $request){
+		return view('welcome');
+	}
 	
 
 }
