2762d39e8fa8ae42727e83789ae81aac93aad35d
Kévin Leprêtre <kevin.lepretre@insa-cvl.fr>
2017-12-07 21:44:46 +0100

    base bootstrap

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -3,10 +3,17 @@
         <meta charset="utf-8">
         <title>Safety - @yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+        <script src="/js/jquery.js" charset="utf-8"></script>
+        <script src="/js/bootstrap.min.js" charset="utf-8"></script>
+
 
     </head>
     <body>
-
+        <div class="navbar">
+          @include('layout.navbar')
+        </div>
         <div class="container">
             @yield('content')
         </div>
