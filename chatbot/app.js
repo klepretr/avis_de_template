0b1e0e4780604233cc9e6c413f71bdcf19bb3d15
julien <julien.rudeau@laposte.net>
2017-12-07 23:27:44 +0100

    requete de la meteo pour la ville

--- chatbot/app.js
+++ chatbot/app.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var websocket = require('socket.io')(server);
 var fs = require('fs');
+var http = require('http');
 
 server.listen(8080);
 
@@ -15,4 +16,26 @@ app.get('/chatbot',function(req,res){
 websocket.on('connection', function (socket) {
   console.log('Un client est connecté');
   socket.emit('message', 'Vous êtes conecté');
+
+  socket.on('ville', function(ville) {
+    console.log(ville);
+    var options = {
+      hostname: 'api.openweathermap.org'
+      ,path: ('/data/2.5/forecast?q=' + ville + '&appid=265c305f780d3efb25d5757f73ea550d')
+      ,method: 'GET'
+      ,headers: { 'Content-Type': 'application/json' }
+    };
+    
+    var req = http.get(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (data) {
+           console.log(data); 
+           var meteo = JSON.parse(data);
+           
+      });
+    }); 
+  });
+
+  
 });
+
