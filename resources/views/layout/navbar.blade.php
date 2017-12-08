f23566c6e4969cb63a385697697f63b91d77d94e
esapin <etienne.sapin@insa-cvl.fr>
2017-12-08 03:23:30 +0100

    Navbar imp

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,6 +1,7 @@
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-fixed-top" id="mainNav">
   <div class="container">
     <div class="navbar-header">
+      <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
