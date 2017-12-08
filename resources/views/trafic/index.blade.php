32b96cae8e170a3940290437b2ddf43a15788bd0
jiroco <jean.savoldelli@insa-cvl.fr>
2017-12-08 07:29:50 +0100

    good

--- resources/views/trafic/index.blade.php
+++ resources/views/trafic/index.blade.php
@@ -24,7 +24,7 @@
   				</div>
   				<div class="form-group">
   					<div class="col-sm-10">
-  						<a href="#" id="trafic_submit" class="btn btn-primary btn-xl control-label col-sm-2" onclick="this.forms['myForm'].submit()">Chercher</a> 
+  						<input type="submit" name="Chercher"> 
   					</div>
   				</div>
 			</form>	
