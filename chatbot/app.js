c7a1e69d530c4ea64fac08acb2aa21ff2f28316c
julien <julien.rudeau@laposte.net>
2017-12-08 02:50:11 +0100

    deleting modifcation in app.js

--- chatbot/app.js
+++ chatbot/app.js
@@ -33,44 +33,8 @@ var dataset = ["bonjour", "salut", "coucou"];
 websocket.on('connection', function (socket) {
   console.log('Un client est connecté');
   socket.emit('message', 'Vous êtes conecté');
-
-  socket.on('ville', function(ville) {
-    //console.log(ville);
-    var options = {
-      hostname: 'api.openweathermap.org'
-      ,path: ('/data/2.5/forecast?q=' + ville + '&appid=265c305f780d3efb25d5757f73ea550d&units=metric&lang=fr')
-      ,method: 'GET'
-      ,headers: { 'Content-Type': 'application/json' }
-    };
-    
-    var req = http.get(options, function(res) {
-      res.setEncoding('utf8');
-      var buffers = [];
-      res
-        .on('data', function(chunk) {
-          buffers.push(new Buffer(chunk));
-        })
-        .on('end', function() {
-          var meteo = JSON.parse(Buffer.concat(buffers).toString());
-          console.log(meteo.city.name);
-          var unJour = []
-          var jours = []
-          for ( var k = 0; k < 4; k++) {
-            for( var compteur = 0; compteur < 8; compteur++) {              
-              unJour.push([meteo.list[compteur+(k*8)].main.temp, 
-                meteo.list[compteur+(k*8)].weather[0].description, 
-                meteo.list[compteur+(k*8)].dt_txt]);
-            }
-            jours.push(unJour);
-          }
-          console.log(unJour.length);
-          console.log(unJour);
-          //console.log(meteo.list[0].main.temp);
-         // console.log(meteo.list[0].weather[0].description);
-         // console.log(meteo.list[0].dt_txt);
-      }); 
-    });
   });
+
   
 
   
@@ -79,5 +43,5 @@ websocket.on('connection', function (socket) {
     console.log('message reçu : ' + message);
     socket.emit('suggest', compare.findBestMatch(message, villes).bestMatch.target);
   });
-});
+
 
